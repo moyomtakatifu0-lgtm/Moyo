1264d6cf55b4a9bae80544e87ea4fb2928935044
moyomtakatifu0-lgtm <moyomtakatifu0@gmail.com>
2026-06-27 11:16:12 +0300

    Update firebase.js

--- public/app/core/firebase.js
+++ public/app/core/firebase.js
@@ -1,34 +1,39 @@
 /*
 PROJECT: MoyoApp
-FIREBASE v9 CLEAN CORE
+FIREBASE v9 CLEAN CORE (STABLE)
 */
 
-import { initializeApp } 
+import { initializeApp }
 from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
 
-import { getAuth } 
+import { getAuth }
 from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
 
-import { getFirestore } 
+import { getFirestore }
 from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
 
-import { getStorage } 
+import { getStorage }
 from "https://www.gstatic.com/firebasejs/9.23.0/firebase-storage.js";
 
+/* FIREBASE CONFIG */
 const firebaseConfig = {
-  apiKey: "AIzaSyAxXw9qu6XSfARlC1ZlQAtT6wKsDGpPP06A",
-  authDomain: "moyo-app-530ef.firebaseapp.com",
-  projectId: "moyo-app-530ef",
-  storageBucket: "moyo-app-530ef.appspot.com",
-  messagingSenderId: "202818396077",
-  appId: "1:202818396077:web:7767ef6a13cbef78c58b24"
+apiKey: "AIzaSyAxXw9qu6XSfARlC1ZlQAtT6wKsDGpPP06A",
+authDomain: "moyo-app-530ef.firebaseapp.com",
+projectId: "moyo-app-530ef",
+storageBucket: "moyo-app-530ef.appspot.com",
+messagingSenderId: "202818396077",
+appId: "1:202818396077:web:7767ef6a13cbef78c58b24"
 };
 
-/* FIXED: export app */
-export const app = initializeApp(firebaseConfig);
+/* INIT APP */
+const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
+/* SERVICES */
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+/* EXPORTS */
+export { app, auth, db, storage };
 
 console.log("🔥 Firebase v9 initialized OK");
